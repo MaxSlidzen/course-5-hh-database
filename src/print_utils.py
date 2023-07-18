c6bb7eeef6a00448376afc8264269c294b3d44c4
Max Slidzen <slidzon94@gmail.com>
2023-07-18 23:41:46 +0300

    corrected output text

--- src/print_utils.py
+++ src/print_utils.py
@@ -6,7 +6,7 @@ def print_companies_and_vacancies_count(data: list[tuple[str, int]]) -> None:
     """
     for employer in data:
         print(f'В компании {employer[0]} всего открыто {employer[1]} вакансий,'
-              f'Из них {employer[2]} находится в базе данных')
+              f'Из них {employer[2]} находится в базе данных\n')
 
 
 def print_vacancies(data: list[tuple[str, int]]) -> None:
@@ -19,7 +19,7 @@ def print_vacancies(data: list[tuple[str, int]]) -> None:
         print(f'В компанию {vacancy[0]} требуется "{vacancy[1]}" с з/п {vacancy[2]}'
               f'{" " + vacancy[3] if vacancy[2] != 0 else ""}. '
               f'Ссылка на вакансию {vacancy[4]}')
-    print(f'\nПоказано результатов: {len(data)}')
+    print(f'\nПоказано результатов: {len(data)}\n')
 
 
 def print_avg_salary(data: tuple[list[tuple[str, float]], float]) -> None:
@@ -32,4 +32,4 @@ def print_avg_salary(data: tuple[list[tuple[str, float]], float]) -> None:
     for currency in data[0]:
         print(f'Средняя з/п по вакансиям в валюте '
               f'{currency[0]} составляет {int(currency[1])}')
-    print(f'Средняя з/п по всем вакансиям в рублевом эквиваленте составляет {int(data[1])}')
+    print(f'Средняя з/п по всем вакансиям в рублевом эквиваленте составляет {int(data[1])}\n')
