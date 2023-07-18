f9977faf80c4109bde5c03abbaa417204945d6f8
Max Slidzen <slidzon94@gmail.com>
2023-07-18 23:43:06 +0300

    corrected main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.hh_data_utils import get_hh_data
 from src.db_utils import create_database, save_data_to_database
 from src.config import config
-import time
+from src.user_interaction import user_interaction
 
 
 def main():
@@ -10,17 +10,21 @@ def main():
 
     params = config()
 
+    print("Происходит сбор данных, создание базы данных и её заполнение.\n"
+          "Это займет некоторое время, пожалуйста подождите")
+
     hh_data = get_hh_data(employers_names)
 
     create_database('hh_database', params)
+
     save_data_to_database(hh_data, 'hh_database', params)
 
+    # Удаление переменной с большим объемом данных для освобождения памяти при работе программы
     del hh_data
+
+    user_interaction()
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    try:
-        main()
-    finally:
-        print("--- %s seconds ---" % (time.time() - start_time))
+
+    main()
